b857c147bf84b0140633fce0c52a2716e688f554
NitzanHen <nitzan.alpha@gmail.com>
2021-09-03 18:40:30 +0000

    migration scrips almost v1

--- scripts/migrateToNewSchema.js
+++ scripts/migrateToNewSchema.js
@@ -1,9 +1,47 @@
 // @ts-check
 
 import { MongoClient } from 'mongodb';
+import { BlobServiceClient, StorageSharedKeyCredential } from '@azure/storage-blob'
+import chalk from 'chalk';
+const { grey, green } = chalk;
+
+// Init mongo clients
 
 const inputClient = new MongoClient('***REMOVED***');
 const outputClient = new MongoClient('mongodb://host.docker.internal:27017');
+
+// Init azure blob storage
+
+const storageAccount = 'agamimportalnext';
+const storageAccountKey = '***REMOVED***';
+const imageContainerName = 'images';
+
+const sharedKeyCredential = new StorageSharedKeyCredential(storageAccount, storageAccountKey);
+
+const blobServiceClient = new BlobServiceClient(
+  `https://${storageAccount}.blob.core.windows.net`,
+  sharedKeyCredential
+)
+
+const imageStorageClient = blobServiceClient.getContainerClient(imageContainerName);
+
+/** 
+ * @param {string} name
+ * @param {Buffer} data
+ */
+async function uploadImage(name, data) {
+  const blob = imageStorageClient.getBlockBlobClient(name);
+
+  await blob.upload(data, Buffer.byteLength(data), {
+    blobHTTPHeaders: {
+      blobContentType: "image"
+    }
+  });
+
+  return blob.url;
+}
+
+// Main execution
 
 async function main() {
   await Promise.all([
@@ -15,7 +53,18 @@ async function main() {
   const indb = inputClient.db('portalagamimdb');
   const outdb = outputClient.db('portal-playground');
 
-  migrateUsers(indb, outdb);
+  //await imageStorageClient.deleteIfExists();
+  await imageStorageClient.createIfNotExists({
+    access: "blob"
+  });
+
+  //await migrateUsers(indb, outdb);
+  //await migrateApps(indb, outdb);
+  //await migrateGroups(indb, outdb);
+  // await migrateCategories(indb, outdb);
+  // await migrateTags(indb, outdb);
+
+  process.exit(0);
 }
 
 /**
@@ -58,21 +107,150 @@ async function migrateUsers(indb, outdb) {
 
   await outUsers.deleteMany({});
   console.log(`Clearing successful. Inserting new users into the collection...`);
-  
+
   await outUsers.insertMany(newUsers);
   console.log(`Insertion successful.`);
 }
 
 /**
  * @param {import("mongodb").Db} indb 
- * @param {import("mongodb").Db} outdb 
+ * @param {import("mongodb").Db} outdb
  */
-function migrateApps(indb, outdb) {
+async function migrateApps(indb, outdb) {
   const inCatalogItems = indb.collection('catalogitems');
   const outApplications = outdb.collection('applications');
+  await outApplications.deleteMany({});
 
-  
+  const applications = await inCatalogItems.aggregate([
+    {
+      $project: {
+        title: 1,
+        description: 1,
+        url: 1,
+        creationDate: '$release',
+        tags: 1,
+        categories: 1,
+        groups: 1,
+        responsive: 1,
+        image: '$image.data'
+      },
+    }
+  ]).toArray();
+
+  console.log(`Read ${applications.length} applications, including raw image data. uploading images...`);
+
+  let count = 0;
+
+  const appsWithImageURL = applications.map((app, i) => {
+    const img = app.image;
+    const ext = /^data:image\/(\w+);base64,/.exec(img)[1];
+    const content = img.replace(/^data:image\/(\w+);base64,/, '');
+    const filename = `${app._id}.${ext}`;
+
+    const dataBuffer = Buffer.from(content, 'base64');
+    return uploadImage(filename, dataBuffer)
+      .then(imageUrl => {
+        const { image, ...appWithUrl } = app;
+        appWithUrl.imageUrl = imageUrl;
+
+        console.log(`(${grey(++count)}): Uploaded image #${i + 1} for app ${green(app.title)}`);
+
+        return appWithUrl;
+      });
+  });
+
+  const uploadResults = await Promise.allSettled(appsWithImageURL);
+  const newApps = [];
+  const failedUploads = [];
+  for (const result of uploadResults) {
+    if (result.status === 'fulfilled') {
+      newApps.push(result.value);
+    }
+    else {
+      failedUploads.push(result.reason);
+    }
+  };
+
+  console.log(`Finished uploading images. ${newApps.length} images were uploaded successfully, ${failedUploads.length} failed.`)
+
+  const response = await outApplications.insertMany(newApps);
+  console.log(`Inserted ${response.insertedCount} new applications into the out collection.`);
+
+  if (failedUploads.length > 0) {
+    console.log('printing upload errors', ...failedUploads);
+  }
+}
+
+/**
+ * @param {import("mongodb").Db} indb 
+ * @param {import("mongodb").Db} outdb
+ */
+async function migrateGroups(indb, outdb) {
+  const inGroups = indb.collection('groups');
+  const outGroups = outdb.collection('groups');
+
+  const groups = await inGroups.aggregate([
+    {
+      $project: {
+        name: 1
+      }
+    }
+  ]).toArray();
+
+  console.log(`Aggregated ${groups.length} groups. Inserting into new collection...`);
+
+  await outGroups.deleteMany({});
+  const response = await outGroups.insertMany(groups);
+
+  console.log(`Successfully inserted ${response.insertedCount} groups into the new collection`);
+}
+
+/**
+ * @param {import("mongodb").Db} indb 
+ * @param {import("mongodb").Db} outdb
+ */
+ async function migrateCategories(indb, outdb) {
+  const inCategories = indb.collection('categories');
+  const outCategories = outdb.collection('categories');
+
+  const categories = await inCategories.aggregate([
+    {
+      $project: {
+        name: 1
+      }
+    }
+  ]).toArray();
+
+  console.log(`Aggregated ${categories.length} categories. Inserting into new collection...`);
+
+  await outCategories.deleteMany({});
+  const response = await outCategories.insertMany(categories);
+
+  console.log(`Successfully inserted ${response.insertedCount} categories into the new collection`);
+}
+
+/**
+ * @param {import("mongodb").Db} indb 
+ * @param {import("mongodb").Db} outdb
+ */
+ async function migrateTags(indb, outdb) {
+  const inTags = indb.collection('tags');
+  const outTags = outdb.collection('tags');
+
+  const tags = await inTags.aggregate([
+    {
+      $project: {
+        name: 1
+      }
+    }
+  ]).toArray();
+
+  console.log(`Aggregated ${tags.length} tags. Inserting into new collection...`);
+
+  await outTags.deleteMany({});
+  const response = await outTags.insertMany(tags);
+
+  console.log(`Successfully inserted ${response.insertedCount} tags into the new collection`);
 }
 
 main();
-
