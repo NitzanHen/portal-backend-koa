c3551935bc6d8ede1b21201fd10053162c1461b4
NitzanHen <nitzan.alpha@gmail.com>
2021-09-03 15:21:05 +0000

    rename account to storageAccount

--- src/peripheral/storage.ts
+++ src/peripheral/storage.ts
@@ -1,26 +1,26 @@
 import { BlobServiceClient, StorageSharedKeyCredential } from '@azure/storage-blob'
 
-const account = process.env.STORAGE_ACCOUNT_NAME;
-const accountKey = process.env.STORAGE_ACCOUNT_KEY;
+const storageAccount = process.env.STORAGE_ACCOUNT_NAME;
+const storageAccountKey = process.env.STORAGE_ACCOUNT_KEY;
 const imageContainerName = process.env.STORAGE_IMAGE_CONTAINER_NAME;
 
 /**
  * @see https://github.com/Azure/azure-sdk-for-js/blob/main/sdk/storage/storage-blob/samples/typescript/src/basic.ts.
  */
 
-if (!account) {
+if (!storageAccount) {
   console.error('No Blob Storage account name provided; make sure an env variable STOAGE_ACCOUNT_NAME exists with the correct account name.')
   process.exit(1);
 }
-if (!accountKey) {
+if (!storageAccountKey) {
   console.error('No Blob Storage account key provided; make sure an env variable STORAGE_ACCOUNT_KEY exists with the correct account key.')
   process.exit(1);
 }
 
-const sharedKeyCredential = new StorageSharedKeyCredential(account, accountKey);
+const sharedKeyCredential = new StorageSharedKeyCredential(storageAccount, storageAccountKey);
 
 const blobServiceClient = new BlobServiceClient(
-  `https://${account}.blob.core.windows.net`,
+  `https://${storageAccount}.blob.core.windows.net`,
   sharedKeyCredential
 )
 
