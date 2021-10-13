13bab7f578771d29e642803060dea27b63db52b0
Nitzan@agamim <58311807+NitzanHen@users.noreply.github.com>
2021-10-13 10:24:06 +0000

    add validate docs

--- src/middleware/validate.ts
+++ src/middleware/validate.ts
@@ -3,6 +3,15 @@ import { Schema, ZodTypeDef } from 'zod';
 
 const get = (o: any, path: string[]): unknown => path.reduce((acc, key) => acc[key], o);
 
+/**
+ * A validation/parsing middleware.
+ * 
+ * @param schema the Zod schema to parse by.
+ * @param outname the name of the field in which to store the output.
+ * Output is always stored as a field on `ctx.state`, e.g. passing `'app'` will store the output on `ctx.state.app`
+ * @param path the "path" to the data to be parsed, relative to `ctx`. 
+ * For example, `['request', 'body']` tells the middleware to read `ctx.request.body`
+ */
 export const validate = <T, OutName extends string>(
   schema: Schema<T, ZodTypeDef, any>,
   outname: OutName,
